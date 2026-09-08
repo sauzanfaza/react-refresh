1a21399a66e247fe763e6673c82a31f897472ead
Sauzan Faza <sauzanfaza@gmail.com>
2026-09-08 10:27:42 +0700

    useEffect and conditional rendering

--- src/Users.jsx
+++ src/Users.jsx
@@ -4,6 +4,7 @@ export default function Users() {
     const [users, setUsers] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [error, setError] = useState(null)
+    const [showUsers, setShowUsers] = useState(true)
 
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
@@ -23,25 +24,33 @@ export default function Users() {
         <>
         {isLoading && <p>Loading...</p>}
         {error && <p>Something Went Wrong...</p>}
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>id</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user) => (
-                        <tr key={user.id}>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+
+        {/* show user button */}
+        <div className='flex justify-center'>
+            <button 
+            onClick={() => setShowUsers((prev) => !prev)}
+            className='p-4 bg-slate-200 hover:bg-slate-400 border border-slate-400 rounded-lg shadow-md'>
+                {showUsers ? "Hide Users" : "Show Users"}
+            </button>
+        </div>
+
+        <div className='p-4 justify-center mt-4'>
+            <div className='grid grid-cols-3 p-2 bg-slate-200 border border-slate-400 shadow-md rounded-lg'>
+                <div className='text-center'>ID</div>
+                <div className='text-center'>Name</div>
+                <div className='text-center'>Email</div>
+            </div>
+            {showUsers && (
+                users.map((user) => (
+                <div 
+                className='grid grid-cols-3 p-2 my-4 bg-slate-200 border border-slate-400 shadow-md rounded-lg'
+                key={user.id}>
+                    <div className='text-center'>{user.id}</div>
+                    <div className='text-center'>{user.name}</div>
+                    <div className='text-center'>{user.email}</div>
+                </div>
+            ))
+            )}
         </div>
         </>
     )
